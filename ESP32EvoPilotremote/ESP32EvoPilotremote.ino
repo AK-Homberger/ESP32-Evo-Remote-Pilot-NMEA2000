5818e3dfcda30a752c471181454d1862ea780601
AK-Homberger <53138495+AK-Homberger@users.noreply.github.com>
2020-07-24 18:47:21 +0200

    Add files via upload

---
+++ ESP32EvoPilotremote/ESP32EvoPilotremote.ino
@@ -0,0 +1,192 @@
+/*
+  This code is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// NMEA2000 Remote Control for EV-1
+//   - Reads 433 MHz commands via RXB6 receiver
+//   - Sends NMEA2000 messages to EV-1 Course Computer
+
+// Version 0.1, 24.07.2020, AK-Homberger
+
+#define ESP32_CAN_TX_PIN GPIO_NUM_2  // Set CAN TX port to 2 
+#define ESP32_CAN_RX_PIN GPIO_NUM_4  // Set CAN RX port to 4
+
+#define ESP32_RCSWITCH_PIN GPIO_NUM_15  // Set RCSWITCH port to 15 (RXB6 receiver)
+
+#include <Arduino.h>
+#include <N2kMsg.h>
+#include <NMEA2000.h>
+#include <NMEA2000_CAN.h>
+#include <RCSwitch.h>
+
+#include "RaymarinePilot.h"
+#include "N2kDeviceList.h"
+
+RCSwitch mySwitch = RCSwitch();
+
+const unsigned long Key_Minus_1 = 1431001; // Change values to individual values programmed to remote control
+const unsigned long Key_Plus_1 = 1431002;
+const unsigned long Key_Minus_10 = 1431003;
+const unsigned long Key_Plus_10 = 1431004;
+const unsigned long Key_Auto = 1431005;
+const unsigned long Key_Standby = 1431006;
+
+const unsigned long TransmitMessages[] PROGMEM = {126208UL, 0};
+const unsigned long ReceiveMessages[] PROGMEM = {127250L, 65288L, 65379L, 0};
+
+tN2kDeviceList *pN2kDeviceList;
+short pilotSourceAddress = -1;
+
+
+void setup() {
+  uint8_t chipid[6];
+  uint32_t id = 0;
+  int i = 0;
+
+  esp_efuse_read_mac(chipid);
+  for (i = 0; i < 6; i++) id += (chipid[i] << (7 * i));
+
+  // Reserve enough buffer for sending all messages. This does not work on small memory devices like Uno or Mega
+  NMEA2000.SetN2kCANReceiveFrameBufSize(150);
+  NMEA2000.SetN2kCANMsgBufSize(8);
+  // Set Product information
+  NMEA2000.SetProductInformation("00000001", // Manufacturer's Model serial code
+                                 100, // Manufacturer's product code
+                                 "Evo Pilot Remote",  // Manufacturer's Model ID
+                                 "1.0.0.0",  // Manufacturer's Software version code
+                                 "1.0.0.0" // Manufacturer's Model version
+                                );
+  // Set device information
+  NMEA2000.SetDeviceInformation(id, // Unique number. Use e.g. Serial number.
+                                132, // Device function=Analog to NMEA 2000 Gateway. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+                                25, // Device class=Inter/Intranetwork Device. See codes on  http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20&%20function%20codes%20v%202.00.pdf
+                                2046 // Just choosen free from code list on http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf
+                               );
+  // Uncomment 3 rows below to see, what device will send to bus
+  Serial.begin(115200);
+  delay(100);
+
+  mySwitch.enableReceive(ESP32_RCSWITCH_PIN);  // Receiver on GPIO15 on ESP32
+
+  //NMEA2000.SetForwardStream(&Serial);  // PC output on due programming port
+  //NMEA2000.SetForwardType(tNMEA2000::fwdt_Text); // Show in clear text. Leave uncommented for default Actisense format.
+  //NMEA2000.SetForwardOwnMessages();
+
+  // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
+  NMEA2000.SetMode(tNMEA2000::N2km_NodeOnly, 51); //N2km_NodeOnly N2km_ListenAndNode
+  NMEA2000.ExtendTransmitMessages(TransmitMessages);
+  NMEA2000.ExtendReceiveMessages(ReceiveMessages);
+
+  NMEA2000.SetMsgHandler(RaymarinePilot::HandleNMEA2000Msg);
+
+  pN2kDeviceList = new tN2kDeviceList(&NMEA2000);
+  //NMEA2000.SetDebugMode(tNMEA2000::dm_ClearText); // Uncomment this, so you can test code without CAN bus chips on Arduino Mega
+  NMEA2000.EnableForward(false); // Disable all msg forwarding to USB (=Serial)
+  NMEA2000.Open();
+
+  Serial.println((String) "NMEA2000 Open");
+
+  /*
+    while(pilotSourceAddress <= 0){
+      NMEA2000.ParseMessages();
+      pilotSourceAddress = getDeviceSourceAddress("EV-1");
+    }
+  */
+
+  pilotSourceAddress = 0;
+  Serial.println((String) "Found EV-1 Pilot: " + pilotSourceAddress);
+}
+
+
+void loop() {
+  int i;
+  unsigned long key;
+
+  if (mySwitch.available()) {
+
+    key = mySwitch.getReceivedValue();
+
+    if (key == Key_Standby) {
+      Serial.println("Setting PILOT_MODE_STANDBY");
+
+      tN2kMsg N2kMsg;
+      RaymarinePilot::SetEvoPilotMode(N2kMsg, pilotSourceAddress, PILOT_MODE_STANDBY);
+      NMEA2000.SendMsg(N2kMsg);
+    }
+
+    else if (key == Key_Auto) {
+      Serial.println("Setting PILOT_MODE_AUTO");
+
+      tN2kMsg N2kMsg;
+      RaymarinePilot::SetEvoPilotMode(N2kMsg, pilotSourceAddress, PILOT_MODE_AUTO);
+      NMEA2000.SendMsg(N2kMsg);
+    }
+
+    else if (key == Key_Plus_1) {
+      Serial.println("Plus 1");
+
+      tN2kMsg N2kMsg;
+      RaymarinePilot::KeyCommand(N2kMsg, pilotSourceAddress, KEY_PLUS_1);
+      NMEA2000.SendMsg(N2kMsg);
+    }
+
+    else if (key == Key_Plus_10) {
+      Serial.println("Plus 10");
+
+      tN2kMsg N2kMsg;
+      RaymarinePilot::KeyCommand(N2kMsg, pilotSourceAddress, KEY_PLUS_10);
+      NMEA2000.SendMsg(N2kMsg);
+    }
+
+    else if (key == Key_Minus_1) {
+      Serial.println("Minus 1");
+
+      tN2kMsg N2kMsg;
+      RaymarinePilot::KeyCommand(N2kMsg, pilotSourceAddress, KEY_MINUS_1);
+      NMEA2000.SendMsg(N2kMsg);
+    }
+
+    else if (key == Key_Minus_10) {
+      Serial.println("Minus 10");
+
+      tN2kMsg N2kMsg;
+      RaymarinePilot::KeyCommand(N2kMsg, pilotSourceAddress, KEY_MINUS_10);
+      NMEA2000.SendMsg(N2kMsg);
+    }
+
+    i = 0;
+    while (mySwitch.available() && i < 2) {
+      mySwitch.resetAvailable();
+      delay (150);
+      i++;
+    }
+  }
+  NMEA2000.ParseMessages();
+}
+
+
+
+int getDeviceSourceAddress(String model) {
+  if (!pN2kDeviceList->ReadResetIsListUpdated()) return -1;
+  for (uint8_t i = 0; i < N2kMaxBusDevices; i++) {
+    const tNMEA2000::tDevice *device = pN2kDeviceList->FindDeviceBySource(i);
+    if ( device == 0 ) continue;
+
+    String modelVersion = device->GetModelVersion();
+
+    if (modelVersion.indexOf(model) >= 0) {
+      return device->GetSource();
+    }
+  }
+  return -2;
+}
---
+++ ESP32EvoPilotremote/RaymarinePilot.cpp
@@ -0,0 +1,270 @@
+#include "RaymarinePilot.h"
+#include <string.h>
+
+double RaymarinePilot::Heading = 0;
+double RaymarinePilot::Variation = 0;
+
+//destinationAddress muss aus der tN2kDeviceList ausgelesen werden. Beispiel dazu: DeviceAnalyzer.ino
+
+void RaymarinePilot::SetEvoPilotMode(tN2kMsg &N2kMsg, uint8_t destinationAddress, RaymarinePilotModes mode) {
+  N2kMsg.SetPGN(126208UL);
+  N2kMsg.Priority=3;
+  N2kMsg.Destination=destinationAddress;
+  N2kMsg.AddByte(1); // Field 1, 1 = Command Message, 2 = Acknowledge Message...
+  N2kMsg.AddByte(0x63);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0x00);
+  N2kMsg.AddByte(0xf8);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x01);
+  N2kMsg.AddByte(0x3b);
+  N2kMsg.AddByte(0x07);
+  N2kMsg.AddByte(0x03);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x04);
+
+  // 0x00 = standby, 0x40 = auto, 0x0100=vane, 0x0180=track
+  switch (mode) {
+  case PILOT_MODE_STANDBY:
+    N2kMsg.AddByte(0x00);
+    N2kMsg.AddByte(0x00);
+    break;
+  case PILOT_MODE_AUTO:
+    N2kMsg.AddByte(0x40);
+    N2kMsg.AddByte(0x00);
+    break;
+  case PILOT_MODE_WIND:
+    N2kMsg.AddByte(0x00);
+    N2kMsg.AddByte(0x01);
+    break;
+  case PILOT_MODE_TRACK:
+    N2kMsg.AddByte(0x80);
+    N2kMsg.AddByte(0x01);
+    break;   
+  }
+ 
+  N2kMsg.AddByte(0x05);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+
+  /*N2kMsg.Add3ByteInt(126208UL); // Field 2, commanded PGN 3 byte,  LSB is transmitted first  
+  N2kMsg.AddByte(0xf0 | 0x8 ); // Field 3, 0x8 = Do not change priority and field 4 is nmea reserved
+  N2kMsg.AddByte(1); // Field 5, 1 field/value pair
+  N2kMsg.AddByte(4); // Field 6, field 4 to be commanded
+  N2kMsg.Add4ByteUInt(0); // Field 7, value for first pair =0 for reset distance log.*/
+}
+
+void RaymarinePilot::SetEvoPilotCourse(tN2kMsg &N2kMsg, uint8_t destinationAddress, double heading, int change) {
+  double course = heading + change;
+  if ((course) >= 360){
+    course -= 360;
+  } else if ((course) < 0){
+    course += 360;
+  }
+  
+  uint16_t courseRadials10000 = (uint16_t) (DegToRad(course) * 10000); //(newCourse * 174.53); 
+  
+
+  byte byte0, byte1;
+  byte0 = courseRadials10000 & 0xff;
+  byte1 = courseRadials10000 >> 8;
+
+  //01,50,ff,00,f8,03,01,3b,07,03,04,06,00,00
+  
+  N2kMsg.SetPGN(126208UL);
+  N2kMsg.Priority=3;
+  N2kMsg.Destination=destinationAddress;
+  N2kMsg.AddByte(1); // Field 1, 1 = Command Message, 2 = Acknowledge Message...
+  N2kMsg.AddByte(0x50);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0x00);
+  N2kMsg.AddByte(0xf8);
+  N2kMsg.AddByte(0x03);
+  N2kMsg.AddByte(0x01);
+  N2kMsg.AddByte(0x3b);
+  N2kMsg.AddByte(0x07);
+  N2kMsg.AddByte(0x03);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x06);
+  N2kMsg.AddByte(byte0);
+  N2kMsg.AddByte(byte1);
+}
+
+void RaymarinePilot::TurnToWaypointMode(tN2kMsg &N2kMsg, uint8_t destinationAddress){
+ //"01,63,ff,00,f8,04,01,3b,07,03,04,04,81,01,05,ff,ff" 
+  N2kMsg.SetPGN(126208UL);
+  N2kMsg.Priority=3;
+  N2kMsg.Destination=destinationAddress;
+  N2kMsg.AddByte(0x01); // Field 1, 1 = Command Message, 2 = Acknowledge Message...
+  N2kMsg.AddByte(0x63);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0x00);
+  N2kMsg.AddByte(0xf8);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x01);
+  N2kMsg.AddByte(0x3b);
+  N2kMsg.AddByte(0x07);
+  N2kMsg.AddByte(0x03);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x81);
+  N2kMsg.AddByte(0x01);
+  N2kMsg.AddByte(0x05);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+}
+
+void RaymarinePilot::TurnToWaypoint(tN2kMsg &N2kMsg, uint8_t destinationAddress){
+  //00,00,ef,01,ff,ff,ff,ff,ff,ff,04,01,3b,07,03,04,04,6c,05,1a,50"
+  
+  N2kMsg.SetPGN(126208UL);
+  N2kMsg.Priority=3;
+  N2kMsg.Destination=destinationAddress;
+  N2kMsg.AddByte(0x00);
+  N2kMsg.AddByte(0x00);
+  N2kMsg.AddByte(0xef);
+  N2kMsg.AddByte(0x01);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x01);
+  N2kMsg.AddByte(0x3b);
+  N2kMsg.AddByte(0x07);
+  N2kMsg.AddByte(0x03);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x04);
+  N2kMsg.AddByte(0x6c);
+  N2kMsg.AddByte(0x05);
+  N2kMsg.AddByte(0x1a);
+  N2kMsg.AddByte(0x50);
+}
+
+void RaymarinePilot::KeyCommand(tN2kMsg &N2kMsg, uint8_t destinationAddress, uint16_t command){
+  //const key_command = "3b,9f,f0,81,86,21,%s,%s,ff,ff,ff,ff,ff,c1,c2,cd,66,80,d3,42,b1,c8"
+
+  byte commandByte0, commandByte1;
+  commandByte0 = command >> 8;
+  commandByte1 = command & 0xff;
+  
+  N2kMsg.SetPGN(126720UL);
+  N2kMsg.Priority=7;
+  N2kMsg.Destination=destinationAddress;
+
+  N2kMsg.AddByte(0x3b);
+  N2kMsg.AddByte(0x9f);
+  N2kMsg.AddByte(0xf0);
+  N2kMsg.AddByte(0x81);
+  N2kMsg.AddByte(0x86);
+  N2kMsg.AddByte(0x21);
+  //N2kMsg.Add2ByteUInt(command);
+  N2kMsg.AddByte(commandByte0);
+  N2kMsg.AddByte(commandByte1);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xff);
+  N2kMsg.AddByte(0xc1);
+  N2kMsg.AddByte(0xc2);
+  N2kMsg.AddByte(0xcd);
+  N2kMsg.AddByte(0x66);
+  N2kMsg.AddByte(0x80);
+  N2kMsg.AddByte(0xd3);
+  N2kMsg.AddByte(0x42);
+  N2kMsg.AddByte(0xb1);
+  N2kMsg.AddByte(0xc8);
+}
+
+void RaymarinePilot::HandleNMEA2000Msg(const tN2kMsg &N2kMsg) {
+  if(N2kMsg.PGN == 127250L){ //Heading
+    unsigned char SID;
+    tN2kHeadingReference ref;
+    double _Deviation=0;
+    double _Variation;
+    double _HeadingRad;
+    
+    if (ParseN2kHeading(N2kMsg, SID, _HeadingRad, _Deviation, _Variation, ref)) {
+      if ( !N2kIsNA(_Variation) ) Variation=_Variation; // Update Variation
+      if ( !N2kIsNA(_HeadingRad) && !N2kIsNA(Variation) ) _HeadingRad-=Variation;
+      Heading = RadToDeg(_HeadingRad);
+    }
+  }
+  else if(N2kMsg.PGN == 65288L){ //Alarm  
+    unsigned char AlarmCode;
+    unsigned char AlarmGroup;
+    
+    if(ParseN2kAlarm(N2kMsg, AlarmCode, AlarmGroup)){
+      Serial.print("Alarm ");
+      Serial.print(AlarmCode, HEX);
+      Serial.println(AlarmGroup, HEX);
+
+      if(AlarmCode == 0x1d && AlarmGroup == 0x01){
+        Serial.println("Alarm Waypoint");
+      }
+    }
+  }
+
+  else if(N2kMsg.PGN == 65379L){ //Pilot State  
+    unsigned char Mode;
+    unsigned char Submode;
+    
+    if(ParseN2kPiloteState(N2kMsg, Mode, Submode)){
+      Serial.print("Mode / Submode ");
+      Serial.print(Mode, HEX);
+      Serial.print(" ");
+      Serial.println(Submode, HEX);
+
+      if(Mode == 0x00 && Submode == 0x00){
+        //RaymarinePilot::PilotMode = PILOT_MODE_STANDBY;
+        Serial.println("PILOT_MODE_STANDBY");
+      }
+      else if(Mode == 0x40 && Submode == 0x00){
+        //RaymarinePilot::PilotMode = PILOT_MODE_AUTO;
+        Serial.println("PILOT_MODE_AUTO");
+      }
+      
+      else if(Mode == 0x00 && Submode == 0x01){
+        //RaymarinePilot::PilotMode = PILOT_MODE_WIND;
+        Serial.println("PILOT_MODE_WIND");
+      }
+      
+      else if(Mode == 0x81 && Submode == 0x01){
+        //RaymarinePilot::PilotMode = PILOT_MODE_TRACK;
+        Serial.println("PILOT_MODE_TRACK");
+      }
+      
+      else if(Mode == 0x80 && Submode == 0x01){
+        //RaymarinePilot::PilotMode = PILOT_MODE_TRACK;
+        Serial.println("PILOT_MODE_TRACK");
+      }
+      
+    }
+  }
+}
+
+bool RaymarinePilot::ParseN2kPGN65288(const tN2kMsg &N2kMsg, unsigned char &AlarmCode, unsigned char &AlarmGroup) {
+  if (N2kMsg.PGN!=65288L) return false;
+
+  int Index=4;
+
+  AlarmCode = N2kMsg.GetByte(Index);
+  AlarmGroup = N2kMsg.GetByte(Index);
+
+  return true;
+}
+
+bool RaymarinePilot::ParseN2kPGN65379(const tN2kMsg &N2kMsg, unsigned char &Mode, unsigned char &Submode) {
+  Serial.println(N2kMsg.PGN);
+  if (N2kMsg.PGN!=65379L) return false;
+
+  int Index=2;
+
+  Mode = N2kMsg.GetByte(Index);
+  Submode = N2kMsg.GetByte(Index);
+
+  return true;
+}
